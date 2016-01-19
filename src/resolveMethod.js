9792eeef87fddeae6f3f948459c40bf787ad2048
Gajus Kuizinas <gk@anuary.com>
2016-01-19 00:41:37 +0000

    methodMap condition must be placed after the version is matched.

--- src/resolveMethod.js
+++ src/resolveMethod.js
@@ -2,15 +2,15 @@ import methodMap from './methodMap';
 import semver from 'semver';
 
 export default (methodName, version) => {
-    if (!methodMap[methodName]) {
-        throw new Error('lodash method "' + methodName + '" does not exist. If you think it is an error, report it at https://github.com/gajus/babel-plugin-lodash-modularize/issues/1.');
-    }
-
     if (semver.gte(version, '4.0.0')) {
         return 'lodash/' + methodName;
     }
 
     if (semver.gte(version, '3.0.0')) {
+        if (!methodMap[methodName]) {
+            throw new Error('lodash method "' + methodName + '" does not exist. If you think it is an error, report it at https://github.com/gajus/babel-plugin-lodash-modularize/issues/1.');
+        }
+
         return 'lodash/' + methodMap[methodName] + '/' + methodName;
     }
 
